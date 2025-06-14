9ab90ecd5c66cbc08724d5b60d68fe84bc7d64f3
E.on <90399175+DariusBurnete@users.noreply.github.com>
2025-06-14 17:56:39 +0300

    Add files via upload

---
+++ kafka-example/src/main/java/com/example/kafka_example/service/MessageProducer.java
@@ -0,0 +1,19 @@
+package com.example.kafka_example.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageProducer {
+
+    private static final String TOPIC = "string_topic";
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send(TOPIC, message);
+        System.out.println("Message sent: " + message);
+    }
+}
